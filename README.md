8ace60cd501d2c3ae561cb9c01108b2cde6947cf
Colston <bodoycolstond@gmail.com>
2021-09-25 21:00:18 +0800

    Update sonnet-farm-prototype folder
    Fixed code formatting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sonnet-farm
+
 Bootstrap 4 website of a farm located in Amocao, Bayabas 2614 Sablan, Philippines.
--- js/navbar.js
+++ js/navbar.js
@@ -1,35 +1,36 @@
-$(document).ready(function() {
-	
+$(document).ready(function () {
 	// smooth scrolling
-	$(".navbar a").click(function(e) {
+	$(".navbar a").click(function (e) {
 		e.preventDefault();
-	
+
 		document.querySelector(this.hash).scrollIntoView({
-			behavior: "smooth" 
+			behavior: "smooth",
 		});
 	});
-	
+
 	// link highlighting & navbar shadow
 	const linkOne = document.querySelector(".home-link");
 	const linkTwo = document.querySelector(".products-link");
 	const sectionOne = document.querySelector(".home");
-	const sectionOneOptions = {rootMargin: "-200px 0px 0px 0px"};
-	const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
-		entries.forEach(entry => {
+	const sectionOneOptions = { rootMargin: "-200px 0px 0px 0px" };
+	const sectionOneObserver = new IntersectionObserver(function (
+		entries,
+		sectionOneObserver
+	) {
+		entries.forEach((entry) => {
 			if (!entry.isIntersecting) {
 				linkOne.classList.remove("active");
 				linkTwo.classList.add("active");
 				document.querySelector(".home-link").style.color = "";
 				document.querySelector(".navbar").classList.add("navbar-shadow");
-			}
-		
-			else {
+			} else {
 				linkOne.classList.add("active");
 				linkTwo.classList.remove("active");
 				document.querySelector(".navbar").classList.remove("navbar-shadow");
 			}
 		});
-	}, sectionOneOptions);
+	},
+	sectionOneOptions);
 
 	sectionOneObserver.observe(sectionOne);
 });
